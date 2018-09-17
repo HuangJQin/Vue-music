b58921699180e52b68b6094c56bb7d1ee09e0b34
HuangJQin <40905313+HuangJQin@users.noreply.github.com>
2018-09-17 20:26:51 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -118,7 +118,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: './',
 
     /**
      * Source Maps
